ccbd224410c18d1df1b927275635d190cf0af5ef
arthur535 <art22miraqyan@gmail.com>
2022-08-24 16:57:41 +0400

    homework1

---
+++ Person_class_with_move_ctor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+class Person {
+public:
+    Person() = default;
+    Person(int _age, std::string _name) {
+        age = _age;
+        name = _name;
+    }
+    Person(Person&& rhs) {
+        age = rhs.age;
+        name = rhs.name;
+    }
+    void printf() {
+        std::cout << "age = " << age << std::endl;
+        std::cout << "name = " << name << std::endl;
+    }
+private:
+    int age;
+    std::string name;
+};
+int main(){
+    Person first(14, "Armen");
+    first.printf();
+    Person second  = std::move(first);
+    first.printf();
+}
+
