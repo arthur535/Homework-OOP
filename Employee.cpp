e5ec5f0647e82fcf68f563a76d319a45d8dff2f6
arthur535 <art22miraqyan@gmail.com>
2022-08-24 17:57:56 +0400

    homework2

---
+++ Employee.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+class Employee {
+public:
+    static long emp_id;
+    Employee() {++emp_id;}
+    ~Employee() = default;
+public:
+    void set_data(std::string _name) {
+        name = _name;
+    }
+    std::string put_data() {
+        return name;
+    }
+private:
+    std::string name;
+}; long Employee::emp_id = 0;
+
+Employee* array_creation_and_fill(const int size) {
+    Employee* employee = new Employee[size];
+    for (int i = 0; i < size; ++i) {
+        std::string tmp_str;
+        std::cout << "please set " << i+1 << " employee name: ";
+        std::cin >> tmp_str;
+        employee[i].set_data(tmp_str);
+    }
+    return employee;
+}
+void print_employees(Employee* employee, const int size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << "Employee " << i + 1 << " name is " << employee[i].put_data() << std::endl;
+    }
+}
+int main() {
+    const int size = 2;
+    Employee* employee = array_creation_and_fill(size);
+    print_employees(employee, size);
+}
